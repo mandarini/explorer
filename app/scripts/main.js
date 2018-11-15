696909ea92d43359e251a3e306016a1bbe8d6004
katerina <sk.katherine@gmail.com>
2018-11-16 00:19:13 +0200

    pwa btn

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,14 +15,10 @@ butInstall.addEventListener('click', () => {
     return;
   }
   promptEvent.prompt();
-  // Log the result
   promptEvent.userChoice.then((result) => {
     console.log('👍', 'userChoice', result);
-    // Reset the deferred prompt variable, since 
-    // prompt() can only be called once.
+
     window.deferredPrompt = null;
-    // Hide the install button.
-    divInstall.classList.toggle('hidden', true);
   });
 });
 
