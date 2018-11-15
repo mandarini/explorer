1bbaf4dbb8443b6b190b1d73e783d95c066a9993
katerina <sk.katherine@gmail.com>
2018-11-15 20:46:17 +0200

    serviceworker ok

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,31 @@
-// self.addEventListener('install', function(event) {
-//   event.waitUntil(
-//     caches.open('katerina-cache-v1')
-//     .then(function(cache) {
-//       return cache.addAll([
-//         '.',
-//         'index.html',
-//         'styles/main.min.css',
-//         'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
-//       ]);
-//     })
-//   );
-// });
-
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(caches.match(event.request)
-//   .then(function(response) {
-//       if (response) {
-//         return response;
-//       }
-//       return fetch(event.request);
-//     })
-//   );
-// });
-
-
-// self.addEventListener('activate', (event) => {
-//   console.log('👷', 'activate', event);
-//   return self.clients.claim();
-// });
-
-self.addEventListener('install', (event) => {
-  console.log('👷', 'install', event);
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('👷', 'activate', event);
-  return self.clients.claim();
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('katerina-cache-v1')
+    .then(function(cache) {
+      return cache.addAll([
+        '.',
+        '/',
+        'index.html',
+        'styles/main.min.css',
+        'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
-  // console.log('👷', 'fetch', event);
-  event.respondWith(fetch(event.request));
+  event.respondWith(caches.match(event.request)
+  .then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+
+self.addEventListener('activate', {scope: '/explorer'},  function(event) {
+  console.log('Activated', 'activate', event);
+  return self.clients.claim();
 });
